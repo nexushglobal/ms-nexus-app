b1f4c727a1dccdf7c6d09cf8320230bc7c859719
anthoni portocarrero rodriguez <softwaretoni21@gmail.com>
2025-08-21 00:41:12 -0500

    refactor: remove unused providers and exports from CommonModule

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './services/users.service';
-import { MembershipService } from './services/memberships.service';
 
 @Module({
-  providers: [UsersService, MembershipService],
-  exports: [UsersService, MembershipService],
+  providers: [],
+  exports: [],
 })
 export class CommonModule {}
