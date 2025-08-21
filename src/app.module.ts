9ed3c7f0308da333f2fc37dbbb36deb5eb777874
anthoni portocarrero rodriguez <softwaretoni21@gmail.com>
2025-08-21 00:38:11 -0500

    feat: add class-transformer and class-validator dependencies; update app module to include CommonModule; remove unused services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseConfig } from './config/database.config';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       useFactory: () => databaseConfig,
     }),
+    CommonModule
+
   ],
   controllers: [],
   providers: [],
--- src/common/services/payments.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { MessagingService } from 'src/messaging/messaging.service';
-
-@Injectable()
-export class PaymentsService {
-  constructor(private readonly client: MessagingService) {}
-}
